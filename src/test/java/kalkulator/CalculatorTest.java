bb27bb0e39cd75cee7f92479f58b48c8eaf2bb13
Jakub Opólski <j.opolski.480@studms.ug.edu.pl>
2024-10-25 14:11:39 +0200

    zadanie 2

--- src/test/java/kalkulator/CalculatorTest.java
+++ src/test/java/kalkulator/CalculatorTest.java
@@ -31,6 +31,14 @@ public class CalculatorTest {
         assertEquals("Integer.MAX_VALUE + 1", Integer.MIN_VALUE, sut.getState());
     }
 
+    @Test
+    public void testAddMinValue() {
+        Calculator sut = new Calculator();
+        sut.setState(Integer.MIN_VALUE);
+        sut.add(-1); // Overflow test
+        assertEquals("Integer.MIN_VALUE + (-1)", Integer.MAX_VALUE, sut.getState());
+    }
+
 	@Test
     public void testAddZero(){
         Calculator sut = new Calculator();
@@ -82,7 +90,7 @@ public class CalculatorTest {
         Calculator sut = new Calculator();
         sut.setState(2);
         sut.mult(Integer.MAX_VALUE / 2);
-        assertEquals("2 * (Integer.MAX_VALUE / 2)", Integer.MAX_VALUE - 1, sut.getState());
+        assertEquals("3 * Integer.MAX_VALUE", Integer.MAX_VALUE - 1, sut.getState());
     }
 
     @Test
@@ -91,5 +99,114 @@ public class CalculatorTest {
         sut.setState(Integer.MAX_VALUE);
         sut.mult(2);
         assertEquals("Integer.MAX_VALUE * 2", -2, sut.getState());
+    }
+//odejmowanie
+    @Test
+    public void testSubPositive() {
+        Calculator sut = new Calculator();
+        sut.setState(10);
+        sut.sub(3);
+        assertEquals("10 - 3 = 7", 7, sut.getState());
+    }
+
+    @Test
+    public void testSubNegative() {
+        Calculator sut = new Calculator();
+        sut.setState(5);
+        sut.sub(-2);
+        assertEquals("5 - (-2) = 7", 7, sut.getState());
+    }
+
+    @Test
+    public void testSubToZero() {
+        Calculator sut = new Calculator();
+        sut.setState(10);
+        sut.sub(10);
+        assertEquals("10 - 10 = 0", 0, sut.getState());
+    }
+
+    @Test
+    public void testSubFromZero() {
+        Calculator sut = new Calculator();
+        sut.sub(5);
+        assertEquals("0 - 5 = -5", -5, sut.getState());
+    }
+
+    @Test
+    public void testSubFromMinValue() {
+        Calculator sut = new Calculator();
+        sut.setState(Integer.MIN_VALUE);
+        sut.sub(1);
+        assertEquals("Integer.MIN_VALUE - 1", Integer.MAX_VALUE, sut.getState()); // Overflow
+    }
+
+    @Test
+    public void testSubFromMaxValue() {
+        Calculator sut = new Calculator();
+        sut.setState(Integer.MAX_VALUE);
+        sut.sub(1);
+        assertEquals("Integer.MAX_VALUE - 1", Integer.MAX_VALUE - 1, sut.getState());
+    }
+//dzielenie
+    @Test
+    public void testDivByPositive() {
+        Calculator sut = new Calculator();
+        sut.setState(10);
+        sut.div(2);
+        assertEquals("10 / 2 = 5", 5, sut.getState());
+    }
+
+    @Test
+    public void testDivByPositive2() {
+        Calculator sut = new Calculator();
+        sut.setState(10);
+        sut.div(2);
+        assertEquals("11 / 2 = 5", 5, sut.getState());
+    }
+
+    @Test
+    public void testDivByNegative() {
+        Calculator sut = new Calculator();
+        sut.setState(10);
+        sut.div(-2);
+        assertEquals("10 / -2 = -5", -5, sut.getState());
+    }
+
+    @Test
+    public void testDivByOne() {
+        Calculator sut = new Calculator();
+        sut.setState(7);
+        sut.div(1);
+        assertEquals("7 / 1 = 7", 7, sut.getState());
+    }
+
+    @Test
+    public void testDivResultZero() {
+        Calculator sut = new Calculator();
+        sut.setState(1);
+        sut.div(3);
+        assertEquals("1 / 3 = 0 (integer division)", 0, sut.getState());
+    }
+
+    @Test
+    public void testDivMinValueByNegativeOne() {
+        Calculator sut = new Calculator();
+        sut.setState(Integer.MIN_VALUE);
+        sut.div(-1);
+        assertEquals("Integer.MIN_VALUE / -1", Integer.MIN_VALUE, sut.getState());
+    }
+
+    @Test
+    public void testDivMaxValueByMaxValue() {
+        Calculator sut = new Calculator();
+        sut.setState(Integer.MAX_VALUE);
+        sut.div(Integer.MAX_VALUE);
+        assertEquals("Integer.MAX_VALUE / Integer.MAX_VALUE = 1", 1, sut.getState());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testDivByZero() {
+        Calculator sut = new Calculator();
+        sut.div(0);
     }
 }
